0bfd77fd89a1e0c53f48f7d576af54d203b0711d
Augusto Soto <sotoaugustoa@gmail.com>
2022-10-28 01:09:25 -0500

    agente casi terminado, esquiva pa'bajo, arreglar

--- agente_final.py
+++ agente_final.py
@@ -1,7 +1,5 @@
-from email import message
-from os import system
 from turtle import *
-# from tkinter import *
+import tkinter
 from random import randrange
 import random
 import turtle
@@ -19,6 +17,9 @@ window.title("Agente reactivo simple")
 # agent = Turtle()
 
 window.setup(width=900, height=700, startx=None, starty=None)
+# window.setup(startx=None, starty=None)
+
+
 
 # «fastest»: 0
 # «fast»: 10
@@ -29,18 +30,22 @@ window.setup(width=900, height=700, startx=None, starty=None)
 speed(0)
 
 
-number_of_obstacles = 2
-cm = 30 #MEDIDA DE LOS CUADROS
-sx = -400 #SETUP X (Posición inicial del lapicero en X)
-sy = 300 #SETUP Y (Posición inicial del lapicero en Y)
-cx = sx #COORDENADAS DEL LAPIZ EN X
-cy = sy #COORDENADAS DEL LÁPIZ EN Y
 
-# size_of_grid = int(input('Digite la matriz: '))
-# Tamaño de la cuadrícula (20x20)
-size_of_grid = 20 
+size_square = 30
+# Tamaño de la cuadrícula (nxn)
+size_of_grid = 19
 
-c = 1 #AUXILIAR QUE INDICA SI EL AGENTE SE MUEVE A LA DERECHA O IZQUIERDA
+number_of_obstacles = 5
+
+agent_initial_position_X = -400 
+agent_initial_position_Y = -270 
+
+pencil_position_X = agent_initial_position_X #COORDENADAS DEL LAPIZ EN X
+pencil_position_Y = agent_initial_position_Y #COORDENADAS DEL LÁPIZ EN Y
+
+
+
+column = 1 #AUXILIAR QUE INDICA SI EL AGENTE SE MUEVE A LA DERECHA O IZQUIERDA
 co = 0 #VARIABLE AUXILIAR PARA ESQUIVAR
 
 
@@ -56,26 +61,26 @@ def create_obstacles():
     # * obstacle
     color("red") #COLOR DEL LÁPIZ Y RELLENO RESPECTIVAMENTE
     
-    aux = sx     #AUXILIAR PARA GUARDAR COORDENADAS EN X
-    auy = sy     #GUARDA TODAS LAS COORDENADAS POSIBLES EN Y
+    random_num_obstacle_position = 0       #AUXILIAR EN LA ELECCIÓN DE LOS OBSTACULOS ALEATORIOS
+    obstacle_position_X = agent_initial_position_X     #AUXILIAR PARA GUARDAR COORDENADAS EN X
+    obstacle_position_Y = agent_initial_position_Y     #GUARDA TODAS LAS COORDENADAS POSIBLES EN Y
     lx = list()  #GUARDA TODAS LAS COORDENADAS EN X
     ly = list()  #GUARDA TODAS LAS COORDENADAS EN Y
     list_coordinates = list()   #GUARDA TODAS LAS COORDENADAS POSIBLES
-    random_num_obstacle_position = 0       #AUXILIAR EN LA ELECCIÓN DE LOS OBSTACULOS ALEATORIOS
+ 
     list_obstacles = []      #GUARDA LAS COORDENADAS DE LOS OBSTACULOS GENERADOS
     
-
-	#! originalmente 18
+	# ! originalmente 18
     for i in range(size_of_grid-2):
-        aux += cm
-        auy -= cm
-        lx.append(aux)
-        ly.append(auy)
+        obstacle_position_X += size_square
+        obstacle_position_Y += size_square
+        lx.append(obstacle_position_X)
+        ly.append(obstacle_position_Y)
     
     for x in lx:
-        for y in ly:                #CICLO PARA EVITAR QUE SE GENEREN OBSTACULOS EN LA PRIMERA Y ULTIMA FILA
+        for y in ly: #CICLO PARA EVITAR QUE SE GENEREN OBSTACULOS EN LA PRIMERA Y ULTIMA FILA
             list_coordinates.append((x,y)) 
-    #print("LOS ARREGLOS LX Y LY SON: \nLX: ", lx,"\nLY: ", ly,"\nL: ",l)
+    print("LOS ARREGLOS LX Y LY SON: \nLX: ", lx,"\nLY: ", ly,"\nL: ",list_coordinates)
 
 
     for i in range (number_of_obstacles):
@@ -95,23 +100,23 @@ def create_obstacles():
         pendown()
         begin_fill()
         setheading(90)
-        forward(cm)
+        forward(size_square)
         left(90)
-        forward(cm)
+        forward(size_square)
         left(90)
-        forward(cm)
+        forward(size_square)
         left(90)
-        forward(cm)
+        forward(size_square)
         end_fill()
         penup()
 
-    setposition(sx,sy)
+    setposition(agent_initial_position_X,agent_initial_position_Y)
 
-def agente():
+def agent():
 
-    global c
-    global cx
-    global cy
+    global column
+    global pencil_position_X
+    global pencil_position_Y
     global nx
     global ny
     global posix
@@ -119,114 +124,128 @@ def agente():
     color("black", "lime") #COLOR DEL LÁPIZ Y RELLENO RESPECTIVAMENTE
     
     for i in range(size_of_grid):
-        if c == False:
-            c = 1
-            for i in range(size_of_grid):
+        
+        if column == False:
+            column = 1
+            
 
-		# ! Originalmente 21
-                if c == (size_of_grid+1):
+            for i in range(size_of_grid):
+		        # ! originalmente 21
+                if column == (size_of_grid+1):                    
                     break
-                posix = ((round(pos()[0]-cm)),(round(pos()[1])))
-                print("HACIA ESTE: ", c, " POSICIÓN EN X, Y: ", round(pos()[0])," ", round(pos()[1]))
+                posix = ((round(pos()[0]-size_square)),(round(pos()[1])))
+                
+                print ("ACA ESTA EL PINCHE POSIX", posix)
+                
+                print("HACIA ESTE: ", column, " POSICIÓN EN X, Y: ", round(pos()[0])," ", round(pos()[1]))
                 pendown()
                 begin_fill()
                 pintar()
                 end_fill()
-                cx -= cm    
+                pencil_position_X -= size_square    
                 
                 if posix in list_obstacles:
-                    cx += cm
-                    cy -= cm
+                    pencil_position_X += size_square
+                    pencil_position_Y -= size_square
                     penup()
-                    setposition(cx,cy)
+                    setposition(pencil_position_X, pencil_position_Y)
                     pendown()
                     begin_fill()
                     pintar()
                     end_fill()
-                    cx -= cm
-                    setposition(cx, cy)
+                    pencil_position_X -= size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     begin_fill()
                     pintar()
                     end_fill()
-                    cx -= cm
-                    setposition(cx, cy)
+                    pencil_position_X -= size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     begin_fill()
                     pintar()
                     end_fill()
-                    cy += cm
-                    setposition(cx, cy)
+                    pencil_position_Y += size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     co += 1
-                    c += 1
+                    column += 1
                 
-                setposition(cx,cy)
-                c+=1
-            cx += cm
-            cy -= cm
+                setposition(pencil_position_X,pencil_position_Y)
+                column+=1
+            pencil_position_X += size_square
+            pencil_position_Y += size_square
             penup()
-            setposition(cx, cy)
+            setposition(pencil_position_X, pencil_position_Y)
             #print("POSCIÓN FINAL FUERA DEL CICLO: ", c, " POSICIÓN EN X, Y: ", round(pos()[0])," ", round(pos()[1])) #POSCIÓN FINAL FUERA DEL CICLO
-            c = True      
+            column = True  
+                
             
         else:
-            c = 1
+            
+            column = 1
             for i in range(size_of_grid):
 
-		# ! Originalmente 21
-                if c == (size_of_grid+1):
+		# ! originalmente 21
+                if column == (size_of_grid+1):
+                    
                     break
                 
                 #print("C: ", c)
                 penup()
-                setposition(cx,cy)
+                setposition(pencil_position_X,pencil_position_Y)
                 pendown()
                 begin_fill()
                 setheading(90)
                 pintar()
                 end_fill()
-                posix = ((round(pos()[0]+cm)),(round(pos()[1])))
-                cx += cm
-                print("HACIA OESTE: ", c," POSICIÓN EN X, Y: ", round(pos()[0])," ",round(pos()[1]))
-                c+=1
+                posix = ((round(pos()[0]+size_square)),(round(pos()[1])))
+                pencil_position_X += size_square
+                print("HACIA OESTE: ", column," POSICIÓN EN X, Y: ", round(pos()[0])," ",round(pos()[1]))
+                column+=1
+                
                 
                 if posix in list_obstacles:
-                    cx -= cm
-                    cy -= cm
+                    pencil_position_X -= size_square
+                    pencil_position_Y -= size_square
                     penup()
-                    setposition(cx,cy)
+                    setposition(pencil_position_X,pencil_position_Y)
                     pendown()
                     begin_fill()
                     pintar()
                     end_fill()
-                    cx += cm
-                    setposition(cx, cy)
+                    pencil_position_X += size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     begin_fill()
                     pintar()
                     end_fill()
-                    cx += cm
-                    setposition(cx, cy)
+                    pencil_position_X += size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     begin_fill()
                     pintar()
                     end_fill()
-                    cy += cm
-                    setposition(cx, cy)
+                    pencil_position_Y += size_square
+                    setposition(pencil_position_X, pencil_position_Y)
                     co += 1
-                    c += 1
+                    column += 1
                     
-            print("FINAL DE LA FILA: ", c, " POSICIÓN EN X, Y: ", round(pos()[0])," ", round(pos()[1])) #POSCIÓN FINAL FUERA DEL CICLO
-            cx -= cm
-            cy -= cm
+                    
+                    
+                    
+            print("FINAL DE LA FILA: ", column, " POSICIÓN EN X, Y: ", round(pos()[0])," ", round(pos()[1])) #POSCIÓN FINAL FUERA DEL CICLO
+            pencil_position_X -= size_square
+            pencil_position_Y += size_square
             penup()
-            setposition(cx,cy)
-            c = False
+            setposition(pencil_position_X,pencil_position_Y)
+            column = False
+            
+            
 
 def pintar():
-    turtle.forward(cm)
+    turtle.forward(size_square)
     turtle.left(90)
-    turtle.forward(cm)
+    turtle.forward(size_square)
     turtle.left(90)
-    turtle.forward(cm)
+    turtle.forward(size_square)
     turtle.left(90)
-    turtle.forward(cm)
+    turtle.forward(size_square)
     turtle.left(90)
     
 def meta():
@@ -238,7 +257,22 @@ def meta():
     pintar()
     end_fill()
     
+    
+        
+def meta():
+    color("purple", "purple")
+    penup()
+    setheading(90)
+    setposition(140, 270)
+    # setposition(170, 300) #for grid 20
+    begin_fill()
+    pintar()
+    end_fill()
+    
+    
 meta()
 create_obstacles()
-agente()
+agent()
+tkinter.messagebox.showinfo(title="Agente reactivo simple", message="FINIIIIIIIIIIIIIIIIIISH")
 window.mainloop()
+exit()
